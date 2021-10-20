6487ab587a5b84d066e0cea9c07adb8f1b8b98fd
Viktor Kozlov <menya.zovut@mail.ru>
2021-10-20 09:51:58 +0400

    full working version + design

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
 'use strict'
-
+let timer, secondsMeter, amountOfSeconds, audio
+let secondsMeterElement = document.getElementById('seconds-meter')
 let startButton = document.getElementById('start-button')
 startButton.onclick = function() {
-    let amountOfSeconds = document.getElementById('amount-of-seconds').value
-    let audio = document.getElementById('main-audio')
-    let timer = setInterval(function() {
-        audio.play()
+    amountOfSeconds = document.getElementById('amount-of-seconds').value
+    audio = document.getElementById('main-audio')
+    secondsMeterActions()
+    timer = setInterval(function() {
+        secondsMeterActions()
     }, amountOfSeconds * 1000)
+}
+
+function secondsMeterActions() {
+    let startTime = new Date().getTime()
+    let endTime = startTime + amountOfSeconds * 1000
+    clearInterval(secondsMeter)
+    audio.play()
+    secondsMeter = setInterval(function() {
+        let currentTime = new Date().getTime()
+        let timeDifference = endTime - currentTime
+        let diffHours = Math.floor(timeDifference / 3600000)
+        timeDifference -= diffHours * 3600000
+        let diffMinutes = Math.floor(timeDifference / 60000)
+        timeDifference -= diffMinutes * 60000
+        let diffSeconds = Math.floor(timeDifference / 1000)
+        timeDifference -= diffSeconds * 1000
+        let diffMilliseconds = String(timeDifference)[0]
+        let diffString = 
+            `${diffHours}:${diffMinutes}:${diffSeconds}.${diffMilliseconds}`
+        secondsMeterElement.innerHTML = diffString
+    }, 100)
+}
+
+let pauseButton = document.getElementById('pause-button')
+pauseButton.onclick = function() {
+    clearInterval(timer)
+    clearInterval(secondsMeter)
 }
