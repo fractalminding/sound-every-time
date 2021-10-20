d3a09114809fa7cbbf6766c72c1b7aaac690cb9f
Viktor Kozlov <menya.zovut@mail.ru>
2021-10-20 10:00:22 +0400

    0:0:0 -> 00:00:00 format

--- main.js
+++ main.js
@@ -20,14 +20,26 @@ function secondsMeterActions() {
         let currentTime = new Date().getTime()
         let timeDifference = endTime - currentTime
         let diffHours = Math.floor(timeDifference / 3600000)
+        if (String(diffHours).length == 1) {
+            diffHours = '0' + diffHours
+        }
         timeDifference -= diffHours * 3600000
         let diffMinutes = Math.floor(timeDifference / 60000)
+        if (String(diffMinutes).length == 1) {
+            diffMinutes = '0' + diffMinutes
+        }
         timeDifference -= diffMinutes * 60000
         let diffSeconds = Math.floor(timeDifference / 1000)
+        if (String(diffSeconds).length == 1) {
+            diffSeconds = '0' + diffSeconds
+        }
         timeDifference -= diffSeconds * 1000
         let diffMilliseconds = String(timeDifference)[0]
+        if (String(diffMilliseconds).length == 1) {
+            diffMilliseconds = '0' + diffMilliseconds
+        }
         let diffString = 
-            `${diffHours}:${diffMinutes}:${diffSeconds}.${diffMilliseconds}`
+            `${diffHours}:${diffMinutes}:${diffSeconds}`
         secondsMeterElement.innerHTML = diffString
     }, 100)
 }
